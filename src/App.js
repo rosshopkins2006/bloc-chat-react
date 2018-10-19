a3991f8361ce100baacedd58a5ecd00f5d77b9a2
rosshopkins2006 <rosshopkins2006@gmail.com>
2018-10-19 14:43:12 -0400

    Added constructor to app to test if typeError setState was still able to be reproduced

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import './App.css';
 
 class App extends Component {
 
+  constructor (props){
+    super(props);
+      this.states = {
+        ClickedState: ""
+      }
+  }
 
+clicked () {
+  this.SetState({ ClickedState: "Clicked" });
+  console.log("You clicked");
+}
   render() {
     return (
       <div className="App">
         <h1>Bloc Chat</h1>
         <RoomList/>
+        <div>
+          <button onClick={this.clicked}>click me</button>
+          {this.ClickedState}
+        </div>
       </div>
     );
   }
