f99f5a881c540bd0c8390f3d57fd216f6f5f3f17
rosshopkins2006 <rosshopkins2006@gmail.com>
2018-11-01 16:51:51 -0400

    trouble shooting console.log problem

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -7,63 +7,45 @@ class MessageList extends Component {
   constructor(props) {
     super(props);
       this.state = {
+        keys: [],
         messages: [],
         message: '',
         roomKey: '',
-        user: {
-          username: 'jen',
-          content: 'Hello world',
-          sentAt: '2025',
-          roomID: '-LPxNkQ_D8m8OWiPSqNW'
-        }
+        username: 'jen',
+        content: '',
+        sentAt: '2025',
+        roomID: '-LPxNkQ_D8m8OWiPSqNW'
       };
 
-      this.messagesRef = firebase.database().ref().child('rooms').child('-LPxNkQ_D8m8OWiPSqNW');
+      this.messagesRef = firebase.database().ref().child('messages');
     }
-
-    whatUser(){
-      console.log(this.state.user);
-    }
-
 
 componentDidMount() {
-  this.roomKey = this.state.user.roomID;
-
-  const messageRefValue = this.messagesRef.child(this.state.user.roomID)
-
-  messageRefValue.on('child_added', snapshot => {
-  const message = snapshot.val();
-
-  this.setState({ messages: this.state.messages.concat( message ) })
+  this.messagesRef.on('child_added', snapshot => {
+  const messageConst = snapshot.val();
+  messageConst.key = snapshot.key;
+  this.setState({ keys: this.state.keys.concat( messageConst.key ) })
+  this.setState({ messagess: this.state.messages.concat( messageConst ) })
   });
 }
 
 handleSubmit(e) {
-  const firebaseRef = firebase.database().ref('rooms');
-  const firebaseRoom = firebaseRef.child(this.state.user.roomID);
+  this.setState({ message: ''});
+  this.setState({ username: 'jen', });
+  this.setState({ content: this.state.message, });
+  this.setState({ sentAt: '2025', });
+  this.setState({ roomID: '-LPxNkQ_D8m8OWiPSqNW' });
+  firebase.database().ref().child('messages').push({
+            username: 'jen',
+            content: this.state.message,
+            sentAt: '2025',
+            roomID: '-LPxNkQ_D8m8OWiPSqNW'
+          });
   e.preventDefault(); //stops page from rerendering
-  firebase.database().ref().child('rooms').child(this.state.user.roomID).push({ user: this.state.user });
-  this.setState({ user:
-             {username: 'jen',
-             content: '',
-             sentAt: '2025',
-             roomID: '-LPxNkQ_D8m8OWiPSqNW'
-           }});
 }
 
 handleChange(e) {
-  const messageContent = this.state.user.content;
-  this.setState({ messageContent: e.target.value })
-}
-
-handleSubmitRoomKey(e) {
-  e.preventDefault();
-  this.setState({ roomKey: this.roomKey});
-}
-
-handleChangeRoomKey(e) {
-  this.roomKey = e.target.value;
-  this.setState({ roomKey: e.target.value })
+  this.setState({ message: e.target.value })
 }
 
 componentWillUnmount() {
@@ -72,7 +54,7 @@ componentWillUnmount() {
 
   render() {
 
-    const displayRooms = this.state.messages.map((name, index) => {
+    const displayMessages = this.state.messages.map((name, index) => {
       return (
         <li key={index}>{name.name}</li>
       )
@@ -81,27 +63,16 @@ componentWillUnmount() {
   return(
     <section>
         <div>
-        <button onClick={this.whatUser()}>whatUser()</button>
         <div>{this.state.roomKey}</div>
-
           <div className="Message-Container">
-            <ul>          <p>{this.state.user.username}</p>
-                          <p>{this.state.user.content}</p>
-                          <p>{this.state.user.sentAt}</p>
-                          <p>{this.state.user.roomID}</p>
-            {displayRooms}
+            <ul>
+            {displayMessages}
             </ul>
           </div>
 
           <form className="Message-Submit" onSubmit={ (e) => this.handleSubmit(e) }>
           message
             <input type="text" value={this.state.message}  onChange={ (e) => this.handleChange(e) } />
-            <input type="submit"/>
-          </form>
-
-          <form className="Message-Submit" onSubmit={ (e) => this.handleSubmitRoomKey(e) }>
-            roomkey
-            <input type="text" value={this.state.roomKey}  onChange={ (e) => this.handleChangeRoomKey(e) } />
             <input type="submit"/>
           </form>
 
